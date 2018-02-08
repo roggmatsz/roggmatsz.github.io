a8c2e0f2aea5c0a92ddcbc73d2a80fdb5bc7d3d3
Rogger Matamoros <roggmatz@gmail.com>
2018-02-08 10:49:14 -0500

    Mobile checkpoint.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector('.clickable')
-     .addEventListener('click', function() {
-        //toggle ul display
-     });
+    const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const pagetitleHeight = document.querySelector('nav').clientHeight;
+    const greetingHeight = viewportHeight - pagetitleHeight;
+    const greetingDiv = document.querySelector('.hero');
+    greetingDiv.style.height = greetingHeight + 'px';
+    const jobDiv = document.querySelector('.job');
+    jobDiv.style.height = (greetingHeight / 2) + 'px';
+    const contactDiv = document.querySelector('.contact');
+    contactDiv.style.height = (greetingHeight / 2) + 'px';
 });
