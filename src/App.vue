463974ef8f03543ee1d4aa842b8a0a1785a936b8
Rogger Matamoros <roggmatz@gmail.com>
2019-03-03 09:46:42 -0500

    Added initial layout.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </nav>
+    <section class="bio">
+      Bio Goes Here
+    </section>
+    <section class="content">
+      <router-view/>
+      Content Goes Here
+    </section>
   </div>
 </template>
 
@@ -15,15 +21,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  display: flex;
+  height: 100%;
+
+  nav {
+    flex-grow: 0;
+  }
+  .bio {
+    flex-grow: 1;
+  }
+  .content {
+    flex-grow: 2;
   }
 }
 </style>
