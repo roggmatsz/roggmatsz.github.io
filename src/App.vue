00bf5feab531859daea72a0160812d4e45873df7
Rogger Matamoros <roggmatz@gmail.com>
2019-04-13 22:44:21 -0400

    Created initial styling for mobile.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <nav>
-      <img class="logo" src="./assets/logo.png" />
+      <a href="/">
+        <img class="logo" src="./assets/logo.png" />
+      </a>
       <div class="social">
         <a href="https://twitter.com/roggmatz">
           <img src="./assets/twitter-logo.svg" />
@@ -29,6 +31,7 @@ export default { }
 </script>
 
 <style lang="scss">
+@import '../node_modules/bulma/sass/utilities/mixins.sass';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -77,6 +80,28 @@ nav {
     }
     a:last-child {
       margin-bottom: 0;
+    }
+  }
+}
+
+@include touch {
+  #app {
+    flex-direction: column;
+  }
+  nav {
+    flex-direction: row;
+    padding: 0;
+    .social {
+      flex-direction: row;
+      a {
+        margin-bottom: 0;
+        padding: 1rem;
+        border: 1px solid red;
+        img {
+          width: 2rem;
+          height: 2rem;
+        }
+      }
     }
   }
 }
